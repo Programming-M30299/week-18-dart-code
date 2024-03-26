d6ea3577662c58855e50dae8b7303127ab1d5ae4
manighahrmani <mashapicasso@gmail.com>
2024-03-26 15:32:19 +0100

    Add examples of constructors

--- pract18.dart
+++ pract18.dart
@@ -1,3 +1,41 @@
 void main() {
   print('Hello, World!');
 }
+
+class Person {
+  // String name = 'unknown';
+  // int age = 0;
+
+  // Constructor with positional parameters
+  // Person(String inputName, int inputAge) {
+  //   name = inputName;
+  //   age = inputAge;
+  // }
+
+  // String? name;
+  // int? age;
+
+  // Constructor with required named parameters
+  // Person({required String inputName, required int inputAge}) {
+  //   name = inputName;
+  //   age = inputAge + 10;
+  // }
+
+  // String name = 'unknown';
+  // int? age;
+
+  // Constructor with a nullable and a required named parameter
+  // Person({String? inputName, required int inputAge}) {
+  //   age = inputAge;
+  // }
+
+  String? name;
+  int? age;
+
+  // Shorter way to define a constructor
+  Person(this.name, this.age);
+
+  void show() {
+    print('Name: $name, Age: $age');
+  }
+}
