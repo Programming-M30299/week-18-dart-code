34b1dd3fb3f455c9a2328ec338685746a4041de9
ManiGhahremani <mani.ghahremani@port.ac.uk>
2024-04-08 18:21:10 +0100

    Add the shorthand constructor for Car

--- lect18.dart
+++ lect18.dart
@@ -1,22 +1,8 @@
 void main() {}
 
-class Meal {
-  String burger;
-  String drink;
+class Car {
+  String colour;
+  double speed;
 
-  Meal(this.burger, this.drink);
-
-  String toString() {
-    return '$burger, $drink and chips';
-  }
-}
-
-class KidsMeal extends Meal {
-  String toy;
-
-  KidsMeal(String burger, String drink, this.toy) : super(burger, drink);
-
-  String toString() {
-    return '${super.toString()} and $toy';
-  }
+  Car(this.colour, this.speed);
 }
