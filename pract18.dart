239780df6c13603c8fb774d08e941484ca3fbc29
manighahrmani <mashapicasso@gmail.com>
2024-04-09 03:09:44 +0100

    Refactor main function to use Person class in pract18.dart

--- pract18.dart
+++ pract18.dart
@@ -1,36 +1,15 @@
 void main() {
-  Shape s = new Shape(10.0, 20.0);
-  print(s);
-  Circle c = new Circle(100.0, 200.0, 50.0);
-  print(c);
+  Person alice = Person('Alice', 20);
+  print(alice.name);
+  print(alice.age);
 }
 
-class Shape {
-  double x = 0.0;
-  double y = 0.0;
+class Person {
+  String name = 'unknown';
+  int age = 0;
 
-  Shape(this.x, this.y);
-
-  void move(double dx, double dy) {
-    x += dx;
-    y += dy;
-  }
-
-  String toString() {
-    return 'x: $x, y: $y';
-  }
-}
-
-class Circle extends Shape {
-  double radius = 0.0;
-
-  // Circle(double x, double y, this.radius) : super(x, y);
-
-  Circle(double x, double y, double radius) : super(x, y) {
-    this.radius = radius;
-  }
-
-  String toString() {
-    return '${super.toString()}, radius: $radius';
+  Person(String name, int age) {
+    this.name = name;
+    this.age = age;
   }
 }
