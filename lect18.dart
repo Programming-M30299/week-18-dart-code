e162331cf46aff06e864288d3dfd8998be0c6377
manighahrmani <mashapicasso@gmail.com>
2024-04-10 04:14:12 +0100

    Add methods to Car

--- lect18.dart
+++ lect18.dart
@@ -1,8 +1,27 @@
-void main() {}
+void main() {
+  Car myCar = Car('red', 10.0);
+  print(myCar.colour); // red
+  print(myCar.speed); // 10.0
+  myCar.colour = 'blue';
+  print(myCar.colour); // blue
+
+  myCar.accelerate(10);
+  print(myCar.speed); // 20.0
+  myCar.brake();
+  print(myCar.speed); // 0.0
+}
 
 class Car {
   String colour;
   double speed;
 
   Car(this.colour, this.speed);
+
+  void accelerate(double inc) {
+    speed += inc;
+  }
+
+  void brake() {
+    speed = 0;
+  }
 }
