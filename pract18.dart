a94570cac5fbfd99363ccad8fdb45f0822855a96
manighahrmani <mashapicasso@gmail.com>
2024-04-10 04:13:18 +0100

    Add methods to Person

--- pract18.dart
+++ pract18.dart
@@ -1,7 +1,13 @@
 void main() {
   Person alice = Person('Alice', 20);
-  print(alice.name);
-  print(alice.age);
+  alice.age = 21;
+  print('Alice is ${alice.age} years old');
+
+  print('Next year, Alice will be ${alice.ageNextYear()} years old');
+  print('Can Alice vote? ${alice.canVote()}');
+
+  print(alice);
+  print(alice.runtimeType);
 }
 
 class Person {
@@ -11,5 +17,21 @@ class Person {
   Person(String name, int age) {
     this.name = name;
     this.age = age;
+  }
+
+  int ageNextYear() {
+    return age + 1;
+  }
+
+  bool canVote() {
+    if (age >= 18) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  String toString() {
+    return 'Person(name: $name, age: $age)';
   }
 }
